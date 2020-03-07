ad6bff03a9c2dcd0a57247936775b385d766ac89
xinhe0616 <648652898@qq.com>
2020-03-07 12:42:33 +0800

    3/7

--- src/main/java/com/mao/controller/LoginController.java
+++ src/main/java/com/mao/controller/LoginController.java
@@ -19,7 +19,10 @@ public class LoginController {
     @Resource
     private AdminUserService adminUserService;
 
-
+    @RequestMapping("/admin/starter")
+    public String starter(){
+        return "/admin/starter";
+    }
     @RequestMapping({"","/index"})
     public String index(){
         return "index";
@@ -32,23 +35,28 @@ public class LoginController {
     public String adminLogin(
         @RequestParam("name") String name,
         @RequestParam("pwd") String pwd,
+        @RequestParam("verifyCode") String verify,
         HttpSession session
     ){
-            if(StringUtils.isEmpty(name) || StringUtils.isEmpty(pwd))
-        {
+        if(StringUtils.isEmpty(name) || StringUtils.isEmpty(pwd)) {
             session.setAttribute("loginError","用户名或者密码为空");
             System.out.println("用户名或者密码为空");
             return "/login";
         }
-            AdminUser user = adminUserService.loginMysql(name, pwd);
-                System.out.println(user);
-            if (user==null){
+        Object verifyGet = session.getAttribute("verify");
+        if (!verify.equals(verifyGet))
+        {
+            session.setAttribute("loginError","验证码错误");
+            System.out.println("验证码错误");
+            return "/login";
+        }
+        AdminUser user = adminUserService.loginMysql(name, pwd);
+        System.out.println(user);
+        if (user==null){
                 session.setAttribute("loginError","账号或者密码错误");
                 System.out.println("账号或者密码错误");
                 return "/login";
-            }
-
-            return "/index";
-
+        }
+        return "/admin/starter";
     }
 }
